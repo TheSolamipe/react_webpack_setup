a339f4f4aa9957bb6367d824d6e2ef8429b43425
Akinola Solamipe <akinolasolamipe@gmail.com>
2020-11-10 23:57:28 +0100

    Solving babel and config error

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'babel-preset-es2015' from 'C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config'\\n    at Function.resolveSync [as sync] (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\resolve\\\\lib\\\\sync.js:89:15)\\n    at resolveStandardizedName (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:101:31)\\n    at resolvePreset (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:58:10)\\n    at loadPreset (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:77:20)\\n    at createDescriptor (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:154:9)\\n    at C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:50\\n    at Array.map (<anonymous>)\\n    at createDescriptors (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:29)\\n    at createPresetDescriptors (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:101:10)\\n    at presets (C:\\\\Users\\\\HP\\\\Desktop\\\\Programming\\\\React-Webpack-config\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:47:19)\");\n\n//# sourceURL=webpack://react-webpack-config/./src/index.js?");
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("index"));
+
+import React from 'react'
+
